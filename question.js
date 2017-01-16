1d9c6822857a8e5e11262b1fec2dfe87a92ba879
hasna Chaddou <hasnachad@gmail.com>
2017-01-16 16:14:10 +0100

    envoi exos js

---
+++ question.js
@@ -0,0 +1,68 @@
+var convertToCelsius = function(temperature) {
+/*/////////////////   CONSIGNE  //////////////////////////////////
+Concevoir un algorithme capable de convertir une température de degrés Farenheit en degrés Celsius. 
+Pour passer d'une valeur Farenheit à une valeur Celsius, il faut appliquer la règle suivante :
+§         Celsius = (Farenheit - 32) * 5/9
+L'algorithme doit lire la température en degrés Farenheit, et l'afficher en Farenheit et en Celsius.
+*/////////////////////////////////////////////////////////////
+    var convertToCelsius;
+    var Farenheit;
+    convertToCelsius=(Farenheit-32)*5/9;
+    return convertToCelsius;
+}
+
+}
+var majeur = function(age) {
+/*//////////////////   CONSIGNE  /////////////////////////////////
+§	Concevoir un algorithme capable retourner la valeur majeur ou mineur selon l'âge.
+§	18 ou plus majeur sinon mineur
+§   var age = integer;
+*/////////////////////////////////////////////////////////////
+    //ici je met mon code ;)
+function(age){
+    var age;
+    if age >= 18 {
+        return("Vous êtes majeur.")
+    }
+    else {
+        return("Vous êtes mineur.")
+    }
+    return reponse
+}
+
+
+var paye = function(vente) {
+/*///////////////////   CONSIGNE  //////////////////////////////// 
+§         L'employé reçoit 200$ par semaine comme salaire de base.
+§         L'employé reçoit 6% du total des ventes hebdomadaires à prix régulier.
+§         L'employé reçoit 3% du total des ventes hebdomadaires à prix rabais.
+          var vente = {ventesRegulier : 0, ventesRabais : 0};				
+*//////////////////////////////////////////////////////////////
+    
+    //ici je met mon code ;)
+    var salaire=200;
+    var ventesRegulier;
+    var ventesRabais;
+    var paye;
+    function(vente){
+        paye=salaire+(ventesRegulier*6/100)+(ventesRabais*3/100)
+        
+        return paye;
+}
+
+
+    return reponse;
+}
+var cesar = function(crypto, chaine) {
+/*/////////////////////   CONSIGNE  ////////////////////////////////    
+§           On code avec César ;)
+§           le mot a codé est dans str 
+§           la clée de chiffrage est cle
+§			la chaine de caractères utilisé pour codé est dans chaine	
+			var crypto = {str : '', cle : 0};	
+*////////////////////////////////////////////////////////////////
+    
+    //ici je met mon code ;)
+
+    return reponse;
+}
